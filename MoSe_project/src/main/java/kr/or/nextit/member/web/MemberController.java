f3166a66c6aa96888a89ed0de1ce1ae1fd2994ef
pc43 <pc43@nextit.or.kr>
2019-05-13 14:56:50 +0900

    study생성

---
+++ MoSe_project/src/main/java/kr/or/nextit/member/web/MemberController.java
@@ -0,0 +1,20 @@
+package kr.or.nextit.member.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+   
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
+	
+	public String getmemberController(
+			
+			)throws Exception{
+		
+		
+		return"";
+	}
+	
+}
